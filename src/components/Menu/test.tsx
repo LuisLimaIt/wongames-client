e917b9bb39cb477d6c0a590e4f13e37603ece32a
LuisLimaIt <pls.luis@gmail.com>
2021-11-19 12:13:20 -0300

    Teste

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -16,17 +16,17 @@ describe('<Menu />', () => {
   it('should handle the open/close mobile menu', () => {
     renderWithTheme(<Menu />)
 
-    // Selecionar o nosso MenuFull.
+    // Selecionar o nosso MenuFull
     const fullMenuElement = screen.getByRole('navigation', { hidden: true })
 
-    // Verificar se o menu está escondido.
+    // Verificar se o menu está escondido
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
-    // Clicar no botão de abrir o menu e verificar se ele abriu.
+    // Clicar no botão de abrir o menu e verificar se ele abriu
     fireEvent.click(screen.getByLabelText(/open menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('false')
     expect(fullMenuElement).toHaveStyle({ opacity: 1 })
-    // Clicar no botão de fechar o menu e verificar se ele fechou.
+    // Clicar no botão de fechar o menu e verificar se ele fechou
     fireEvent.click(screen.getByLabelText(/close menu/i))
     expect(fullMenuElement.getAttribute('aria-hidden')).toBe('true')
     expect(fullMenuElement).toHaveStyle({ opacity: 0 })
